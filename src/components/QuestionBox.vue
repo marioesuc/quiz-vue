4d4c8a7ebe3abcf2f871231b8b3a5f90ae3dcf52
Mario Esteban Uclés <marioesuc@marios-mbp.home>
2020-02-09 22:56:37 +0100

    feat: implement API call and show the questions on the screen and functionality for next button

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -2,7 +2,7 @@
   <div class="question-box-container">
     <b-jumbotron>
       <template v-slot:lead>
-        Some questions here?
+        {{ currentQuestion.question }}
       </template>
 
       <hr class="my-4" />
@@ -12,7 +12,19 @@
       </p>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  mounted() {
+    console.log(this.currentQuestion)
+  }
+}
+</script>
